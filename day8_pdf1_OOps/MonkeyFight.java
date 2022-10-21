8b87b48e343168f7cfde8b8b7aca1ffe426a8e17
jaga992000github <114407227+jaga992000github@users.noreply.github.com>
2022-10-21 10:14:19 +0530

    Add files via upload

--- day8_pdf1_OOps/MonkeyFight.java
+++ day8_pdf1_OOps/MonkeyFight.java
@@ -1,7 +1,7 @@
 package day8_pdf1_OOps;
 import java.util.*;
 public class MonkeyFight {
-
+	static int secMI=0;//second maximum index for monkey 2
 	public static int findMaxIndex(int[] trees) {
 		int max=0,maxindex=0;
 		int size=trees.length;
@@ -15,37 +15,53 @@ public class MonkeyFight {
 		return maxindex ;
 	}
 	
+	
+	static int maxpath(int [] trees,int MI) {
+		int ans=0;
+		int size=trees.length;
+		int path=0; 
+		
+		
+		int	maxHighPath=0;//max distance from second monkey tree to first monkey ground	System.out.println("Enter size of each trees");	
+		int MHPans=0;
+		
+		if(path<0) {path*=-1;}
+		path=MI-0;
+		maxHighPath=path+trees[0];
+		for(int i=0;i<size;i++) {
+			path=MI-i;
+			if(path<0) {path*=-1;}
+			if(maxHighPath<path+trees[i]) {
+				maxHighPath=path+trees[i];
+				secMI=i;
+				MHPans=maxHighPath;
+			}
+		}ans=MHPans;
+		return ans;
+	}
+	
+	
 	public static void main(String[]args) {
 	Scanner scan=new Scanner(System.in);
 	System.out.println("Enter no of trees");
 	int size =scan.nextInt();
 	int ans=0;
-	int path=0, 
-			MI=0,//maximum index for monkey 1
-			secMI=0,//second maximum index for monkey 2
-			maxHighPath=0;//max distance from second monkey tree to first monkey ground
-	System.out.println("Enter size of each trees");	
-	int[] trees=new int[size];
 	
+	
+	int[] trees=new int[size];
+	System.out.println("Enter the tree high");
 	for(int i=0;i<size;i++) {
 		trees[i]=scan.nextInt();}
 
-	MI =findMaxIndex(trees);
+	int MI =findMaxIndex(trees);//maximum index for monkey 1
 	ans+=trees[MI];						//6 3 2 5 1 8 7 4
+	int temp=trees[MI];					//5 2 4 1 5 5 3 4 3
+	trees[MI]=0;
 	
-	maxHighPath=path+trees[0];
-	for(int i=0;i<size;i++) {
-		path=MI-i;
-		if(path<0) {path*=-1;}
-		if(maxHighPath<path+trees[i]) {
-			maxHighPath=path+trees[i];
-			secMI=i;
-			ans+=maxHighPath;
-		}
-	}
+	ans+=maxpath(trees,MI);
 	
 	
-	System.out.println("1st monkey tree no("+(MI+1)+")= "+trees[MI]+" feet");
+	System.out.println("1st monkey tree no("+(MI+1)+")= "+temp+" feet");
 	System.out.println("2st monkey tree no("+(secMI+1)+")= "+trees[secMI]+" feet");
 	System.out.println("Max Distance between them "+ans);
 }
